baa1d120cfef84df30ef62eba366f8cae0a1b080
antoine <toitoinebzh@laposte.net>
2020-01-11 09:51:07 +0100

    resoud erreur suppression #4

--- supprimer.php
+++ supprimer.php
@@ -6,7 +6,12 @@ require "controleur/Controleur.php";
 
 if (isset($_GET["id"]) or isset($_POST["id"]))
 {
-	$id=htmlspecialchars($_GET["id"]);
+	if (isset($_GET["id"])){
+		$id=htmlspecialchars($_GET["id"]);	
+	}
+	else {
+		$id=htmlspecialchars($_POST["id"]);	
+	}
 	pageSupprimer($id);
 }
 else 
