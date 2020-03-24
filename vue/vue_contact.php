37303eb7b7b18cb2cd3200739e7d143d0144d135
antoine <toitoinebzh@laposte.net>
2020-03-24 09:59:12 +0100

    update pieddepage et pagecontact

--- vue/vue_contact.php
+++ vue/vue_contact.php
@@ -3,6 +3,10 @@
 
 <article>
 
+<p>
+Ceci est la version 0.1.0 de <a href="https://framagit.org/anto1ne/petitesannonces">PetitesAnnonces</a>.
+</p>
+
 <p>Ce site à été réalisé à l'aide de <a href="http://bluefish.openoffice.nl/">Bluefish</a>.
 <!-- et <a href="https://fluxbb.fr/">Fluxbb</a>.</p>
 <p>Pour toutes demandes d'informations, veuillez utiliser le <a href="../forums/index.php">Forum</a>. 
@@ -10,8 +14,11 @@
 </p>
 
 <p>Pour plus de renseignements ou pour signaler un bug, rendez-vous sur le site internet de 
-<a href="https://framagit.org/toitoinebzh/petitesannonces/">PetitesAnnonces.</a>
+<a href="https://framagit.org/anto1ne/petitesannonces">PetitesAnnonces</a> et sur le 
+<a href="https://framagit.org/anto1ne/petitesannonces/-/wikis/home">Wiki.</a>
 </p>
+
+
 
 </article>
 
